05d89ecf3878089dab3981aee7e8eae927aa78e7
NENTI NURNANINGSIH <nentinur300901@gmail.com>
2022-04-13 16:04:06 +0700

    add send in PasswordResetTokenServiceImpl

---
+++ src/main/java/com/nenti/service/implementation/PasswordResetTokenServiceImpl.java
@@ -0,0 +1,16 @@
+package com.nenti.service.implementation;
+
+import com.nenti.repository.PasswordResetTokenRepository;
+import com.nenti.service.framework.PasswordResetTokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PasswordResetTokenServiceImpl implements PasswordResetTokenService {
+    private final PasswordResetTokenRepository passwordResetTokenRepository;
+
+    @Autowired
+    public PasswordResetTokenServiceImpl(PasswordResetTokenRepository passwordResetTokenRepository) {
+        this.passwordResetTokenRepository = passwordResetTokenRepository;
+    }
+}
