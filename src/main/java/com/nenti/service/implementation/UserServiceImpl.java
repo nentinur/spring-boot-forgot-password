9fb9fc689e5feb0a206e6b7e0a76eaef855b401f
NENTI NURNANINGSIH <nentinur300901@gmail.com>
2022-04-13 16:10:29 +0700

    add UserServiceImpl

---
+++ src/main/java/com/nenti/service/implementation/UserServiceImpl.java
@@ -0,0 +1,24 @@
+package com.nenti.service.implementation;
+
+import com.nenti.repository.RoleRepository;
+import com.nenti.repository.UserRepository;
+import com.nenti.service.framework.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
+
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder,
+                           RoleRepository roleRepository) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.roleRepository = roleRepository;
+    }
+}
