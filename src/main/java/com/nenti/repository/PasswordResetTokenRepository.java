8e3f7bd9ff70f778857bad13564de61d6e884cde
NENTI NURNANINGSIH <nentinur300901@gmail.com>
2022-04-13 15:39:40 +0700

    add PasswordResetTokenRepository

---
+++ src/main/java/com/nenti/repository/PasswordResetTokenRepository.java
@@ -0,0 +1,10 @@
+package com.nenti.repository;
+
+import com.nenti.entity.PasswordResetToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+    Optional<PasswordResetToken> findByToken(String token);
+}
