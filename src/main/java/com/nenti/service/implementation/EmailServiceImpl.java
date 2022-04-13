2ef02405d6f34236fbc230bc023ee75b9313fd39
NENTI NURNANINGSIH <nentinur300901@gmail.com>
2022-04-13 15:57:01 +0700

    add EmailServiceImpl

---
+++ src/main/java/com/nenti/service/implementation/EmailServiceImpl.java
@@ -0,0 +1,20 @@
+package com.nenti.service.implementation;
+
+import com.nenti.service.framework.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+
+@Service
+public class EmailServiceImpl implements EmailService {
+    private final JavaMailSender mailSender;
+    private final SpringTemplateEngine templateEngine;
+
+    @Autowired
+    public EmailServiceImpl(JavaMailSender mailSender, SpringTemplateEngine templateEngine) {
+        this.mailSender = mailSender;
+        this.templateEngine = templateEngine;
+    }
+
+}
