74b4665faeb890d8af3942dd1cd5794e42605d2f
NENTI NURNANINGSIH <nentinur300901@gmail.com>
2022-04-13 15:41:16 +0700

    add RoleRepository

---
+++ src/main/java/com/nenti/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.nenti.repository;
+
+import com.nenti.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
