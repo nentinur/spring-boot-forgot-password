a13b3e87c7c80c723714467346826e81ccd105e3
NENTI NURNANINGSIH <nentinur300901@gmail.com>
2022-04-13 15:49:38 +0700

    add PasswordResetTokenService

---
+++ src/main/java/com/nenti/service/framework/PasswordResetTokenService.java
@@ -0,0 +1,9 @@
+package com.nenti.service.framework;
+
+import com.nenti.entity.PasswordResetToken;
+
+public interface PasswordResetTokenService {
+    PasswordResetToken findByToken(String token);
+
+    PasswordResetToken save(PasswordResetToken passwordResetToken);
+}
