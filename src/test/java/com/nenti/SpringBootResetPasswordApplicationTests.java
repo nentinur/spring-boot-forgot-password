e7ee79ae65919d518aa4c2680c76ea10f713e2bc
NENTI NURNANINGSIH <nentinur300901@gmail.com>
2022-04-13 13:46:18 +0700

    Initial commit

---
+++ src/test/java/com/nenti/SpringBootResetPasswordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nenti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootResetPasswordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
