2f950d658e0d90a9f9f06c7a5f4f52f3258bd602
NENTI NURNANINGSIH <nentinur300901@gmail.com>
2022-04-13 19:04:16 +0700

    add ResetPasswordController

---
+++ src/main/java/com/nenti/controller/ResetPasswordController.java
@@ -0,0 +1,24 @@
+package com.nenti.controller;
+
+import com.nenti.service.framework.PasswordResetTokenService;
+import com.nenti.service.framework.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/reset-password")
+public class ResetPasswordController {
+    private final PasswordResetTokenService tokenService;
+    private final MessageSource messageSource;
+    private final UserService userService;
+
+    @Autowired
+    public ResetPasswordController(PasswordResetTokenService tokenService, MessageSource messageSource,
+                                   UserService userService) {
+        this.tokenService = tokenService;
+        this.messageSource = messageSource;
+        this.userService = userService;
+    }
+}
