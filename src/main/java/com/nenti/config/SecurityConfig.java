c96f3e20f29e283a25d863e6593b895bd3f122fb
NENTI NURNANINGSIH <nentinur300901@gmail.com>
2022-04-13 17:43:13 +0700

    add ForgotPasswordController

--- src/main/java/com/nenti/config/SecurityConfig.java
+++ src/main/java/com/nenti/config/SecurityConfig.java
@@ -1,7 +1,7 @@
 package com.nenti.config;
 
 import com.nenti.service.framework.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -11,9 +11,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
---
+++ src/main/java/com/nenti/controller/ForgotPasswordController.java
@@ -0,0 +1,27 @@
+package com.nenti.controller;
+
+import com.nenti.service.framework.EmailService;
+import com.nenti.service.framework.PasswordResetTokenService;
+import com.nenti.service.framework.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/forgot-password")
+public class ForgotPasswordController {
+    private final UserService userService;
+    private final MessageSource messageSource;
+    private final PasswordResetTokenService passwordResetTokenService;
+    private final EmailService emailService;
+
+    @Autowired
+    public ForgotPasswordController(UserService userService, MessageSource messageSource,
+                                    PasswordResetTokenService passwordResetTokenService, EmailService emailService) {
+        this.userService = userService;
+        this.messageSource = messageSource;
+        this.passwordResetTokenService = passwordResetTokenService;
+        this.emailService = emailService;
+    }
+}
